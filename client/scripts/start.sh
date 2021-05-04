06ef3f06ab27b8c535fa3423734e17957023a659
Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
2021-05-03 22:03:16 -0300

    Criando o script para o client

---
+++ client/scripts/start.sh
@@ -0,0 +1,20 @@
+set -e
+
+cd client/
+yarn install
+
+if [ -n $CLIENT_ENV ]
+then
+  if [ "$CLIENT_ENV" = "development" ]
+  then
+    quasar dev
+  elif [ "$CLIENT_ENV" = "test" ]
+  then
+    yarn test:unit:coverage
+  elif [ "$CLIENT_ENV" = "style" ]
+  then
+    yarn lint
+  else
+    echo "Unknown CLIENT_ENV value..."
+  fi
+fi
